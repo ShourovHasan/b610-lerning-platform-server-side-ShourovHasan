c996c4296bc9242c75870f4a09890de8b8bedfb0
ShourovHasan <shourovhasan@gmail.com>
2022-10-25 13:06:22 +0600

    categories added

--- index.js
+++ index.js
@@ -5,37 +5,37 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-// const categories = require('./data/categories.json');
-// const courses = require('./data/courses.json');
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('AIUB Portal API Running');
 });
 
-// app.get('/courses-categories', (req, res) => {
-//     res.send(categories);
-// });
+app.get('/courses-categories', (req, res) => {
+    res.send(categories);
+});
 
-// app.get('/courses', (req, res) => {
-//     res.send(courses);
-// });
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
 
-// app.get('/category/:id', (req, res) => {
-//     const id = req.params.id;
-//     if (id === '08') {
-//         res.send(courses);
-//     }
-//     else {
-//         const categoryNews = courses.filter(nw => nw.category_id === id);
-//         res.send(categoryNews);
-//     }
-// })
-// app.get('/courses/:id', (req, res) => {
-//     const id = req.params.id
-//     const selectedNews = courses.find(nw => nw.id === id);
-//     res.send(selectedNews);
-//     // console.log(req.params.id);
-// })
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(courses);
+    }
+    else {
+        const categoryNews = courses.filter(nw => nw.category_id === id);
+        res.send(categoryNews);
+    }
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id
+    const selectedNews = courses.find(nw => nw.id === id);
+    res.send(selectedNews);
+    // console.log(req.params.id);
+})
 
 app.listen(port, () => {
     console.log('AIUB Portal Server running on port', port);
